7c4287e5a112f0657c834ffe07fbbfb9541e1ecd
MashooqAli9140 <Faizsde9140@gmail.com>
2024-11-30 17:28:18 +0530

    i bodparser

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
+
+
+//using body parser for getting from URL body
+app.use(bodyparser.json());
+
+
+
+
+
 
 PORT = process.env.PORT || 4000;
-
 app.listen( PORT , () => {
     console.log( `server running on ${PORT}`);
 }) 
