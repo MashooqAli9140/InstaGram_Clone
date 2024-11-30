33ca7b69066ee6bfca3ccaa5fcb504dc4ec78e62
MashooqAli9140 <Faizsde9140@gmail.com>
2024-11-30 16:15:53 +0530

    created signup form

--- client/src/componants/HomePage/Homepage.jsx
+++ client/src/componants/HomePage/Homepage.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Homepage = () => {
+      const [opensignform , setopensignupform ] = useState("none");
+      const [openloginform , setopenloginform ] = useState("block");
+
 
 
   return (
     <div>
-      <h1> instagram</h1>
-      <form type="submit"> 
-        <input type="text" placeholder='username or email' />
-        <input type="password" placeholder='password' />
-        <button type='submit'> Log in</button>
-      </form>
-      <p> or </p>
-     <a href="/"> forgot password ? </a>
+   //created login form
+      <div id="loginformdiv" style={ { display: `${openloginform}` }} >
+        <h1> instagram</h1>
+        <form type="submit">
+          <input type="text" placeholder="username or email" />
+          <input type="password" placeholder="password" />
+          <button type="submit"> Log in</button>
+        </form>
+        <p> or </p>
+        <a href="/"> forgot password ? </a>
 
-     <p> don't have an account ?</p>
-     <button> Sign up </button>
-     <h5> Get the app </h5>
-     <a href="/"> Get app on GPLAY </a>
-     <br />
-     <a href="/"> Get app on APP STORE </a>
+        <p> don't have an account ?</p>
+        <button onClick={ (e) => { setopensignupform("block") , setopenloginform("none")   }} > Sign up </button>
+        <h5> Get the app </h5>
+        <a href="/"> Get app on GPLAY </a>
+        <br />
+        <a href="/"> Get app on APP STORE </a>
+      </div>
+
+   //created signup form
+      <div id="loginformdiv" style={ { display: `${opensignform}` }} >
+        <h1> register</h1>
+        <form type="submit">
+          <input type="text" placeholder="username" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button type="submit"> signup </button>
+        </form>
+        <p> already have an account ?</p>
+        <button onClick={ (e) => { setopensignupform("none") , setopenloginform("block")   }} > Login </button>
+      </div>
+
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
