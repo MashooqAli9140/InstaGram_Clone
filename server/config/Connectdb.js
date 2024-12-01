ef4e73bf2c8b6995be2d54dc7e13b24796818e23
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-01 15:31:25 +0530

    installed mongoose and connected to mongodb

---
+++ server/config/Connectdb.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect( process.env.DATABASE_URI , {
+
+        })
+        console.log( "MONGODB CONNECTED SUCCESFULLY");
+    } catch (error) {
+        console.log("error while connecting to DB")
+    }
+}
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const connectDB = require("./config/Connectdb.js");
 
 
 //using body parser for getting from URL body
 app.use(bodyparser.json());
+
+//connect to DB
+connectDB();
+
+
+
+
+
+//geting post req for signupdata
+app.post("/signup" , async( req , res ) => {
+    
+})
+
+
+
 
 
 
