954c9ab17ea29692068ee48edbee91ebc35f13c1
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-01 16:20:47 +0530

    updated cors and add post req for signupdata

--- server/server.js
+++ server/server.js
@@ -6,12 +6,13 @@ const mongoose = require("mongoose");
 const connectDB = require("./config/Connectdb.js");
 const cors = require("cors");
 const UsersignupData = require("./model/UsersignupData.js");
+const bcrypt = require("bcrypt");
 
 //using body parser for getting from URL body
 app.use(bodyparser.json());
 
 // Enable CORS with specified origin for frontend communication
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 
 //connect to DB
@@ -25,15 +26,22 @@ connectDB();
 app.post("/signup" , async( req , res ) => {
     const { username , password , email } = req.body;
     if( !username || !password , !email ) return res.status(400).json( {msge : "all feilds are required"});
+    console.log( username , password , email);
 
    try {
-      //hash the pw     
-
+      //hash the pw
+      const HashedPW = await bcrypt.hash( password , 10 );
+      const SaveUserData = await UsersignupData.create( {
+        username,
+        HashedPW,
+        email
+      })
+      console.log(SaveUserData);
+    return res.status(201).json({msge:"user Signup Data stored Success" , SaveUserData })
    } catch (error) {
-    
+    console.log(error);
+    return res.status(400).json({msge:"user Signup Data stored Success"})
    }
-
-
 
 })
 
