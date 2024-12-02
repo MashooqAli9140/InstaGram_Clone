d900fc35cfe6a1a39bcd3ba3958e50e4b5759c79
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-02 18:10:23 +0530

    navbar created

--- client/src/componants/HomePage/LoginUserPage/LoginUser.jsx
+++ client/src/componants/HomePage/LoginUserPage/LoginUser.jsx
@@ -8,11 +8,11 @@ const LoginUser = () => {
            <div id='maindashboard'>
                  <div id='dashboard-Navbar'>
                       <div id='navlogo'>
-                          <h1>Instagram</h1>
+                          <h1 style={{ fontFamily:"allura , cursive" , fontSize:"36px"}}>Instagram</h1>
                       </div>
                       <div id='nav-right'>
-                            <button> <i class="fa-solid fa-plus"></i> </button>
-                            <button> <i class="fa-regular fa-heart"></i> </button>
+                            <button id='plusbtn'> <i class="fa-solid fa-plus fa-3x" style={{ color:"white"}}></i> </button>
+                            <button id='heart'> <i class="fa-regular fa-heart fa-3x" style={{ color:"white"}}></i> </button>
                       </div>
                  </div>
            </div>
