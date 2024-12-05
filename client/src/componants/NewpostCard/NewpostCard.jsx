e0d2b7cb103c93ba21884f5fda7127e7cf2ee4b9
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-05 13:41:48 +0530

    new post username section and img section done

--- client/src/componants/NewpostCard/NewpostCard.jsx
+++ client/src/componants/NewpostCard/NewpostCard.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import "./NewpostCard.css"
+import DP from "/src/images/dp.jpg";
+
 
 const NewpostCard = () => {
   return (
     <div id='new-post-card-bg'>
-         <h1> post card </h1>
+         {/* // USERNAME SECTION  */}
+      <div id='post-username-section'>
+             <div id='post-profile'>
+                   <img src= {DP} id='postprofile-img' alt="porfile pic" />
+             </div>
+             <div id='post-username-editbtn'>
+                 <p> Username </p>  
+                 <h2> --- </h2> 
+             </div>
+      </div>
+
+
+         {/* // IMAGE SECTION */}
+          <div id='post-img-div'>
+                 <img src={DP} id='post-img' alt="post-img" />
+          </div>
+
     </div>
   )
 }
