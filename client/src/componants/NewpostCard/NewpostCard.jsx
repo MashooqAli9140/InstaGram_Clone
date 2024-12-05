304fe4885ddbc8ccf6faffd8bf2e06f1b13bd2e2
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-05 14:01:10 +0530

    like, comment and share section done

--- client/src/componants/NewpostCard/NewpostCard.jsx
+++ client/src/componants/NewpostCard/NewpostCard.jsx
@@ -17,10 +17,22 @@ const NewpostCard = () => {
              </div>
       </div>
 
-
          {/* // IMAGE SECTION */}
           <div id='post-img-div'>
                  <img src={DP} id='post-img' alt="post-img" />
+          </div>
+
+         {/* // LIKE , COMMENT SECTION */}
+          <div id='like-btn-div'>
+                  <div id='like-cmnt-section'> 
+                  <i class="fa-regular fa-heart fa-2x"></i>
+                  <i class="fa-regular fa-comment fa-2x"></i>
+                  <i class="fa-solid fa-share-nodes fa-2x"></i>
+                  </div>
+
+                  <div>
+                     <p> save post </p>
+                  </div>
           </div>
 
     </div>
