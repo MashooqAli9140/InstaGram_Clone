63cc73f6dd0b2f4686f7aec066815ab46ba08229
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-07 13:27:03 +0530

    comment section navbar css done

--- client/src/componants/CommentSection/CommentSection.jsx
+++ client/src/componants/CommentSection/CommentSection.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
-import "./CommentSection.css"
+import "./CommentSection.css";
+import { useNavigate, useParams } from "react-router-dom";
+
 
 const CommentSection = () => {
+    const navigate = useNavigate();
+    const { post_id , username , loginUser } = useParams();
+
+
+
   return (
     <div id='comment-bg'>
      <div id='comment-dashboard'>
-       <h1> Comment page </h1>
+        
+        <div id='comment-nav'>
+            <button style={{ background:"none" , color:"white" , border:"none" , outline:"none" , cursor:"pointer" }} onClick={ () => navigate(`/${loginUser}`) }>
+                 <div> <i class="fa-solid fa-arrow-left fa-2x"></i>  </div>
+            </button>
+
+             <h3 style={{ fontWeight:"500"}}> Comments </h3>
+             <div> 
+                <i style={{ color:"#121212", fontWeight:"100" }} class="fa-solid fa-arrow-left fa-2x"></i> 
+            </div>
+        </div>
+
+
      </div>
 
     </div>
