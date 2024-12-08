e4516a55dcb80a586a61ac9006195be2a93e4c63
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-08 12:42:13 +0530

    followby array added in usersignup data schema

--- server/model/UsersignupData.js
+++ server/model/UsersignupData.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const Signupdata = new Schema( {
       username: { type:String , required: true , unique: true},
       password: { type: String , required: true},
-      email: { type: String , required: true}
+      email: { type: String , required: true},
+      followby: [{ type: String }],
  },
  {
     collection: "SIGNUP_USER_DATA"
