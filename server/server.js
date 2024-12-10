2458ac54889f1ca8f22e96ed143db07b5b42dbbb
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-11 03:06:53 +0530

    updated static file dist

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(cors({ origin: "https://instagram-clone-by-faiz.onrender.com" }));
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // Serve static files from the `dist` directory
-app.use(express.static(path.join(__dirname, "client" , "dist" )));
+app.use(express.static(path.join(__dirname, "dist" )));
 
 
 // Set security headers
