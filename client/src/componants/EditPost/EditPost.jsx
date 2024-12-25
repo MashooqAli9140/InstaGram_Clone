8d0d52eb3e8bb9417fbfab285415814d91474c11
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-25 21:19:00 +0530

    dist updated

--- client/src/componants/EditPost/EditPost.jsx
+++ client/src/componants/EditPost/EditPost.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 
 const EditPost = () => {
   const { posttext , id } = useParams();
-  const [newpostText, setnewpostText] = useState("posttext"); 
+  const [newpostText, setnewpostText] = useState(posttext); 
 
   //SEND NEW POST FUNCTION FOR SENDING NEW POST DATA TO BE INLCUDING IMAGE
   async function SendNewPost(e) {
