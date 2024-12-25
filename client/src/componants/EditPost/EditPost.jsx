1ac0e708fbbc74cae74e2e87306eac9824692cb2
MashooqAli9140 <Faizsde9140@gmail.com>
2024-12-25 20:46:04 +0530

    use state imported

--- client/src/componants/EditPost/EditPost.jsx
+++ client/src/componants/EditPost/EditPost.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 
 const EditPost = () => {
   const [newpostText, setnewpostText] = useState("");
@@ -24,7 +25,7 @@ const EditPost = () => {
     }
   }
 
-  
+
   return (
     <div>
               <div
